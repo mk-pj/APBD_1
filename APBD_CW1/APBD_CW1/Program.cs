e0981d0c762a46edb01a3a8f56faa9f1e94ff06c
s31209 <s31209Wpjwstk.edu.pl>
2025-03-07 13:19:35 +0100

    Adding super class

--- APBD_CW1/APBD_CW1/Program.cs
+++ APBD_CW1/APBD_CW1/Program.cs
@@ -1,5 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using APBD_CW1;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("First change");
+
+SuperClass c = new SuperClass(5);
+
+Console.WriteLine(c.getNum());
---
+++ APBD_CW1/APBD_CW1/SuperClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APBD_CW1
+{
+    internal class SuperClass {
+        int num;
+        public SuperClass(int num) {
+            this.num = num;
+        }
+
+        public int getNum() { return num; }
+    }
+}
